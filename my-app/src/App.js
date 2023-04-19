fb1546a028da029fab32f064ee0737fb0d5a6edb
Sean Do <Sean.q.d215@gmail.com>
2023-04-18 23:44:40 -0700

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,9 @@ import { useNavigate } from 'react-router-dom';
 import './App.css';
 import { Login } from './Pages/Login';
 import { Signup } from './Pages/Signup';
-//import { Contributions } from './Pages/Contributions';
+
+
+
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
